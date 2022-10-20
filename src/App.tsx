7c72f883427c2aa33bd91cb622321c57a824a3f3
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2022-10-20 16:17:22 -0300

    styles: add basic global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { GlobalStyles } from './styles/global';
+
 export function App() {
-  return <h1>Hello, Sidebar</h1>;
+  return (
+    <>
+      <GlobalStyles />
+      <h1>Hello, Sidebar</h1>
+    </>
+  );
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    background-color: #333;
+    color: #fff;
+
+    a {
+      text-decoration: none;
+      color: inherit;
+    }
+
+    button {
+      cursor: pointer;
+    }
+  }
+`;
