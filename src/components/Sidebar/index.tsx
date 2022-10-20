c98dd086ec8d4d70d28d9d00602e00d185966216
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2022-10-20 17:41:23 -0300

    feat: add github and gitlab buttons on the sidebar component

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,12 +1,18 @@
 import { HTMLAttributes } from "react";
+import { FiGithub, FiGitlab } from 'react-icons/fi';
 import { Container } from "./styles";
 
 interface SidebarProps extends HTMLAttributes<HTMLElement> {}
 
-export function Sidebar({ children, ...rest }: SidebarProps) {
+export function Sidebar(props: SidebarProps) {
   return (
-    <Container {...rest}>
-      {children}
+    <Container {...props}>
+      <button type="button">
+        <FiGithub />
+      </button>
+      <button type="button">
+        <FiGitlab />
+      </button>
     </Container>
   );
 }
--- src/components/Sidebar/styles.ts
+++ src/components/Sidebar/styles.ts
@@ -4,8 +4,24 @@ export const Container = styled.aside`
   position: absolute;
   top: 48px;
   left: 48px;
+  display: flex;
+  flex-direction: column;
   width: 50px;
   height: 80%;
+  overflow: hidden;
   border-radius: 8px;
   background-color: #222;
+
+  button {
+    padding: 16px 0;
+    border: none;
+    background-color: transparent;
+    color: blueviolet;
+
+    transition: background-color 200ms;
+
+    &:hover {
+      background-color: #111;
+    }
+  }
 `;
